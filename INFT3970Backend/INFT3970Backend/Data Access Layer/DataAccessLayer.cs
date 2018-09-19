16e6529f650a202ae5e4e1257da9fe69710c0bf3
DAVID-DESKTOP\david <c3260947@uon.edu.au>
2018-09-20 09:24:07 +1000

    Added in login access connection string to SQL server

--- INFT3970Backend/INFT3970Backend/Data Access Layer/DataAccessLayer.cs
+++ INFT3970Backend/INFT3970Backend/Data Access Layer/DataAccessLayer.cs
@@ -11,6 +11,7 @@ namespace INFT3970Backend.Data_Access_Layer
     {
         //The connection string to the database
         protected const string ConnectionString = @"Server=.\SQLEXPRESS;Database=udb_CamTag;Trusted_Connection=True;";
+        //protected const string ConnectionString = @"Server=.\SQLEXPRESS;Database=udb_CamTag;User Id=DataAccessLayerLogin;Password=test";
 
         //The error message returned when an exception is thrown when tryig to connect to the DB and run a stored procedure
         protected const string DatabaseErrorMSG = "An error occurred while trying to connect to the database.";
